9b2934560c0151367b1cf8eb50e8568c79fdbab4
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-08-07 10:38:03 +0900

    posts#createでデータベースに値を保存するように変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,19 @@
+# coding: utf-8
 class PostsController < ApplicationController
 	def new
 	end
 
 	def create
-		render text: params[:post].inspect
+		@post = Post.new(post_params)
+
+		@post.save
+		redirect_to @post
+	end
+
+private
+	def post_params
+		# require(キー) キーが存在している場合、それを返す
+		# permit(キー) 指定したキーのみを返す
+		params.require(:post).permit(:title, :text)
 	end
 end
